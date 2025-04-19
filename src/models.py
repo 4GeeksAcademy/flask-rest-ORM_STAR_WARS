71225e22214520012951a36bf5e7a1336766f061
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-04-19 19:50:50 +0000

    firts

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -16,4 +16,83 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100),nullable=False)
+    climate: Mapped[str] = mapped_column(String(100))
+    population: Mapped[str] = mapped_column(String(50))
+
+    characters: Mapped[list["Character"]] = relationship("Character", back_populates="homeworld")
+    residents: Mapped[list["People"]] = relationship("People", back_populates="homeworld")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population
+
+        }
+class Character(db.Model):
+    __tablename__ = "character"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    gender: Mapped[str] = mapped_column(String(20))
+    birth_year: Mapped[str] = mapped_column(String(20))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+
+    homeworld: Mapped["Planet"] = relationship("Planet", back_populates="characters")
+    vehicles: Mapped[list["Vehicle"]] = relationship("Vehicle", back_populates="pilot")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "planet_id": self.planet_id
+        }
+class Vehicle(db.Model):
+    __tablename__ = "vehicle"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    model: Mapped[str] = mapped_column(String(100))
+    pilot_id: Mapped[int] = mapped_column(ForeignKey("character.id"))
+
+    pilot: Mapped["Character"] = relationship("Character", back_populates="vehicles")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "pilot_id": self.pilot_id
+        }
+class People(db.Model):
+    __tablename__ = "people"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    gender: Mapped[str] = mapped_column(String(20))
+    height: Mapped[str] = mapped_column(String(10))
+    mass: Mapped[str] = mapped_column(String(10))
+    birth_year: Mapped[str] = mapped_column(String(20))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+
+    homeworld: Mapped["Planet"] = relationship("Planet", backref="residents")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass,
+            "birth_year": self.birth_year,
+            "planet_id": self.planet_id
         }
